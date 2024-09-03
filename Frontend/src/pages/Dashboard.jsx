b458e0ab1d636ef1dff61efcdf35a035c50523b4
Vasudev Patel <22it118@charusat.edu.in>
2024-09-03 21:02:55 +0530

    assets commit

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
-
+import './path/to/app-assets/vendors/css/vendors.min.css'; // Adjust path as necessary
+import './path/to/app-assets/css/bootstrap.css';
+import './path/to/app-assets/css/bootstrap-extended.css';
+import './path/to/app-assets/css/colors.css';
+import './path/to/app-assets/css/components.css';
+import './path/to/app-assets/css/themes/dark-layout.css';
+import './path/to/app-assets/css/themes/bordered-layout.css';
+import './path/to/app-assets/css/themes/semi-dark-layout.css';
+import './path/to/app-assets/css/core/menu/menu-types/horizontal-menu.css';
+import './path/to/assets/css/style.css';
 
 
 const Dashboard = ({ username }) => {
