f7d50cf0703c9f64bbefc5cd3956ee2c5814c24f
Vasudev Patel <22it118@charusat.edu.in>
2024-09-04 17:38:23 +0530

    commit

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,27 +1,16 @@
 import React from 'react';
-import './path/to/app-assets/vendors/css/vendors.min.css'; // Adjust path as necessary
-import './path/to/app-assets/css/bootstrap.css';
-import './path/to/app-assets/css/bootstrap-extended.css';
-import './path/to/app-assets/css/colors.css';
-import './path/to/app-assets/css/components.css';
-import './path/to/app-assets/css/themes/dark-layout.css';
-import './path/to/app-assets/css/themes/bordered-layout.css';
-import './path/to/app-assets/css/themes/semi-dark-layout.css';
-import './path/to/app-assets/css/core/menu/menu-types/horizontal-menu.css';
-import './path/to/assets/css/style.css';
 
 
 const Dashboard = ({ username }) => {
   return (
     <div className="dashboard">
-      {/* <h1>Welcome to Cyber Craft</h1>
+      <h1>Welcome to Cyber Craft</h1>
       {username ? (
         <p>Welcome, {username}!</p>
       ) : (
         <p>Welcome to Cyber Craft! Please log in to access your personalized dashboard.</p>
-      )} */}
+      )}
 
-      <h1>Welcome to Cyber Craft</h1>
       
 
     </div>
